e35121a8e7e76f86721d6bfe24ee8d1bbc36250e
justwlocke <justlocke1998@gmail.com>
2021-09-01 13:55:46 -0500

    Setup the basic template for the miner profession.

---
+++ UntitledTownSim/Assets/Scripts/MinerPandaBT.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Panda;
+
+public class MinerPandaBT : PandaBTScripts
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Find and set the agents destination to their assigned mine entrance.
+    /// </summary>
+    [Task]
+    public void FindMineEntrance()
+    {
+
+    }
+
+    /// <summary>
+    /// Mine for whatever resource the mine has by moving the agent inside of the mine.
+    /// </summary>
+    [Task]
+    public void Mine()
+    {
+
+    }
+
+    /// <summary>
+    /// Find and set the agents destination to an avaliable drop off
+    /// </summary>
+    [Task]
+    public void FindRockDropOff()
+    {
+
+    }
+
+    //Transfer the stored rock to the drop off.
+    [Task]
+    public void DropOffRock()
+    {
+
+    }
+
+
+}
